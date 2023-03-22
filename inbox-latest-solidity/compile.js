8aedcc4709d578b1abc5aa556a116a84406001d2
Wilker <wilker.soh@cognizant.com>
2023-03-22 20:37:29 +0800

    Lesson completed

---
+++ inbox-latest-solidity/compile.js
@@ -0,0 +1,31 @@
+/**
+ * use v16.14.0 node
+ */
+const path = require("path");
+const fs = require("fs");
+const solc = require("solc");
+
+const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
+const source = fs.readFileSync(inboxPath, "utf8");
+
+const input = {
+  language: "Solidity",
+  sources: {
+    "Inbox.sol": {
+      content: source,
+    },
+  },
+  settings: {
+    outputSelection: {
+      "*": {
+        "*": ["*"],
+      },
+    },
+  },
+};
+
+console.log(JSON.parse(solc.compile(JSON.stringify(input))));
+
+module.exports = JSON.parse(solc.compile(JSON.stringify(input))).contracts[
+  "Inbox.sol"
+].Inbox;
